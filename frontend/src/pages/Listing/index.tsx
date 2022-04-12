47a8b82235bc5921357187ee6ac2699316d9e2d8
zepinha <japatecnologico@gmail.com>
2022-04-12 11:59:54 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,25 +22,21 @@ function Listing() {
 
   useEffect(() => {
     axios
-      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
-      .then((response) => {
-        const data = response.data as MoviePage;
-        setPage(data);
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+        .then((response) => {
+          const data = response.data as MoviePage;
+          setPage(data);
       });
   }, [pageNumber]);
 
-  const movie = {
-    id: 1,
-    image:
-      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5,
-  };
+   const handlePageChange = (newPageNumber : number) => {
+    setPageNumber(newPageNumber);
+  }
 
   return (
     <>
-      <Pagination />
+      <Pagination page={page} onChange={handlePageChange} />
+
       <div className="container">
         <div className="row">
           {page.content.map((movie) => (
